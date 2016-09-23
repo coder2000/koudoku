79b791272a7601e2db0e5f8945f9dc1804973e79
Dieter Lunn <coder2000@gmail.com>
2016-09-22 21:36:04 -0500

    Added redcarpet renderer

--- app/helpers/koudoku/application_helper.rb
+++ app/helpers/koudoku/application_helper.rb
@@ -9,6 +9,27 @@ module Koudoku
       interval = %w(month year week 6-month 3-month).include?(plan.interval) ? plan.interval.delete('-') : 'month'
       I18n.t("koudoku.plan_intervals.#{interval}")
     end
+    
+    def markdown(text)
+      options = {
+        filter_html:     true,
+        hard_wrap:       true, 
+        link_attributes: { rel: 'nofollow', target: "_blank" },
+        space_after_headers: true, 
+        fenced_code_blocks: true
+      }
+
+      extensions = {
+        autolink:           true,
+        superscript:        true,
+        disable_indented_code_blocks: true
+      }
+
+      renderer = Redcarpet::Render::HTML.new(options)
+      markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+      markdown.render(text).html_safe
+    end
 
     # returns TRUE if the controller belongs to Koudoku
     # false in all other cases, for convenience when executing filters 
